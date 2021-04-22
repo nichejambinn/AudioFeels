f755d47aee2220337b0d5ca42cbf177cc1783ead
JacobwBruce <jacobwbruce@hotmail.com>
2021-04-22 13:15:09 -0400

    removing unused import

--- src/main/java/ca/moodyjay/audio/beans/User.java
+++ src/main/java/ca/moodyjay/audio/beans/User.java
@@ -1,7 +1,5 @@
 package ca.moodyjay.audio.beans;
 
-import java.util.*;
-
 import javax.persistence.*;
 import lombok.*;
 
@@ -10,16 +8,16 @@ import lombok.*;
 @Data
 @Builder
 @Entity
-@Table(name="user", schema="public")
+@Table(name = "user", schema = "public")
 public class User {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	private String username;
 	private String password;
-	
-	@ManyToOne(cascade=CascadeType.ALL)//, fetch=FetchType.EAGER)
-	private Role role;	
+
+	@ManyToOne(cascade = CascadeType.ALL) // , fetch=FetchType.EAGER)
+	private Role role;
 }
